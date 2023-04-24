9f67d287f2fe5237b15d7b93f363f19046c08d2d
Replit user <>
2023-04-24 12:24:32 +0000

    Add Mongoose Schema and Model

--- myApp.js
+++ myApp.js
@@ -3,6 +3,17 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI ,{ useNewUrlParser: true, useUnifiedTopology: true })
 let Person;
 
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true, 
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+Person = mongoose.model("Person", personSchema);
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
